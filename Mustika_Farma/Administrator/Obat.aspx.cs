cf14be31b3bb5684022ca87872dfb6693b3eb9f6
danisandika <danisandika1@gmail.com>
2019-11-25 14:41:05 +0700

    del

--- Mustika_Farma/Administrator/Obat.aspx.cs
+++ Mustika_Farma/Administrator/Obat.aspx.cs
@@ -70,10 +70,4 @@ public partial class Administrator_Obat : System.Web.UI.Page
         conn.Close();
         //loadData();
     }
-
-
-    protected void DDLJenisObat_SelectedIndexChanged(object sender, EventArgs e)
-    {
-
-    }
 }
